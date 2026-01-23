412388fc5997d7ee579aa032ab6da5c669845493
Shivang Dey <shivangdey491@gmail.com>
2026-01-23 06:37:46 +0530

    Create floor_in_a_sorted_array.c

---
+++ floor_in_a_sorted_array.c
@@ -0,0 +1,16 @@
+int findFloor(int* arr, int n, int x) {
+    int low = 0, high = n - 1;
+    int ans = -1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] <= x) {
+            ans = mid;  
+            low = mid + 1;  
+        } else {
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
